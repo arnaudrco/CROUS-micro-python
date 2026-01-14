0bfe4a768d3d994c1ad4293692b9320e1e8613ca
reichart <90700891+arnaudrco@users.noreply.github.com>
2026-01-14 06:42:52 +0100

    Add files via upload

---
+++ esp32S3/jack-camille/jack-camille.ino
@@ -0,0 +1,95 @@
+// TEST OK sur carte LOLIN S3 MIni, le 22/12/2024
+//
+// Source :  https://www.xtronical.com/i2sinternetradio/
+// IDE Arduino 2.3.4
+// Core  ESP32 d'Espressif en version 3.1.0  
+// Bibliothèque : https://github.com/schreibfaul1/ESP32-audioI2S  ( en VERSION  3.0.12)
+// Selectionner pour Partition Scheme  une option laissant au moins 2MB pour le code
+
+#include "Arduino.h"
+#include "WiFi.h"
+#include "Audio.h"
+
+/* GPIOs utilisés ici pour l'ampli BF I2S  MAX98357A, autres choix possibles
+#define I2S_DOUT      10  // DIN connection 
+#define I2S_BCLK      11  // Bit clock 
+#define I2S_LRC       12  // Left Right Clock
+*/
+#define GND  1
+#define I2S_LRC  2
+#define I2S_DOUT  3
+#define I2S_BCLK 4
+#define SCK  5
+
+Audio audio;
+
+String ssid =     "NUMERICABLE-63";
+String password = "camille01";
+
+
+void setup() {
+
+  
+  pinMode(GND, OUTPUT); // board prise jack" "PCM5100" GND
+  digitalWrite(GND, LOW);
+  pinMode(SCK, OUTPUT); // board prise jack" "PCM5100" GND
+  digitalWrite(SCK, LOW);
+  delay(100);
+  
+  Serial.begin(115200);
+  WiFi.disconnect();
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid.c_str(), password.c_str());
+  while (WiFi.status() != WL_CONNECTED) delay(1500);
+  audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
+  audio.setVolume(6); // 0...21
+
+  audio.connecttohost("https://icecast.radiofrance.fr/monpetitfranceinter-midfi.aac");
+
+  //audio.connecttohost("http://icecast.radiofrance.fr/fipjazz-midfi.mp3");
+  //audio.connecttohost("http://icecast.radiofrance.fr/fipworld-midfi.mp3");
+  //audio.connecttohost("http://icecast.radiofrance.fr/fip-midfi.mp3");
+  //audio.connecttohost("http://icecast.radiofrance.fr/franceinfo-midfi.mp3");
+  //audio.connecttohost("http://tsfjazz.ice.infomaniak.ch/tsfjazz-high.mp3");
+}
+
+
+void loop()
+{
+  audio.loop();
+}
+
+// optional
+void audio_info(const char *info) {
+  Serial.print("info        "); Serial.println(info);
+}
+void audio_id3data(const char *info) { //id3 metadata
+  Serial.print("id3data     "); Serial.println(info);
+}
+void audio_eof_mp3(const char *info) { //end of file
+  Serial.print("eof_mp3     "); Serial.println(info);
+}
+void audio_showstation(const char *info) {
+  Serial.print("station     "); Serial.println(info);
+}
+void audio_showstreaminfo(const char *info) {
+  Serial.print("streaminfo  "); Serial.println(info);
+}
+void audio_showstreamtitle(const char *info) {
+  Serial.print("streamtitle "); Serial.println(info);
+}
+void audio_bitrate(const char *info) {
+  Serial.print("bitrate     "); Serial.println(info);
+}
+void audio_commercial(const char *info) { //duration in sec
+  Serial.print("commercial  "); Serial.println(info);
+}
+void audio_icyurl(const char *info) { //homepage
+  Serial.print("icyurl      "); Serial.println(info);
+}
+void audio_lasthost(const char *info) { //stream URL played
+  Serial.print("lasthost    "); Serial.println(info);
+}
+void audio_eof_speech(const char *info) {
+  Serial.print("eof_speech  "); Serial.println(info);
+}
