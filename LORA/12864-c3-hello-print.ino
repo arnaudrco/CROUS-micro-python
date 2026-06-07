589b67a2cda652aa96e00569cf9e0946b75786c7
reichart <90700891+arnaudrco@users.noreply.github.com>
2026-06-07 09:21:47 +0200

    Refactor display initialization and output logic
    Updated display variable name and modified display logic.

--- LORA/12864-c3-hello-print.ino
+++ LORA/12864-c3-hello-print.ino
@@ -1,12 +1,12 @@
 /*
-
- HELLO 
+ HELLO
+ fonction type FEED pour afficher des lignes 
 */
 
 #include <Arduino.h>
 #include <U8x8lib.h>
 
-U8X8_ST7567_ENH_DG128064I_4W_SW_SPI  u8x8( /* clock=*/ 6, /* data=*/ 7, /* cs=*/ 4, /* dc=*/ 5, /* reset=*/ 3);
+U8X8_ST7567_ENH_DG128064I_4W_SW_SPI  display( /* clock=*/ 6, /* data=*/ 7, /* cs=*/ 4, /* dc=*/ 5, /* reset=*/ 3);
 
 int i,j; // affiche un compteur
 
@@ -15,24 +15,25 @@ void setup(void){
     Serial.begin(115200);
  delay(100);
      Serial.println(F("7567a"));
-  u8x8.begin();  delay(100); // initDisplay
-  u8x8.setPowerSave(0);
+  display.begin();  delay(100); // initDisplay
+  display.setPowerSave(0);
 //  u8x8.setContrast(200);
-  u8x8.setFont(u8x8_font_amstrad_cpc_extended_r);
-u8x8.setCursor(0, 0);
-u8x8.println("Hello 7567a");  
-u8x8.println("0123456789012345");  
+  display.setFont(u8x8_font_amstrad_cpc_extended_r);
+
+    display.setInverseFont(1);
+display.println("LORA 7567");  
     delay(500);
-    u8x8.setCursor(0, 0);
+    display.setInverseFont(0);
+        display.setCursor(0, 1);
 }
 
 void loop(void)
 {
-if (i++ >= 8) { // pas plus de 8 lignes
-  u8x8.setCursor(0, 0); // retour ligne 0
+if (i++ >= 5) { // pas plus de 5 lignes
+  display.setCursor(0, 1); // retour ligne 0
   i=0;
 }
-u8x8.print("Line :");  
-u8x8.println(j++); 
+display.print("Ligne :");  
+display.println(j++); 
   delay(500);
 }
