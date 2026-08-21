7c733c96545ea14f0e3c76c079a3a05e1702df93
reichart <90700891+arnaudrco@users.noreply.github.com>
2026-08-21 16:46:11 +0200

    Refactor loop for LED blinking and data transmission
    Updated loop to toggle LED and send data every 2 seconds.

--- HC-12/HC-12.ino
+++ HC-12/HC-12.ino
@@ -1,16 +1,18 @@
+// Balise toutes les 2 secondes
 #include <SoftwareSerial.h>
 SoftwareSerial HC12(10, 11); // Configuration des broches TX et RX
-int pot = A0;
 
+
+  
 void setup() {
+    pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);   // Communication série avec l'ordinateur
   HC12.begin(9600);     
-  pinMode(pot, INPUT);
+
 }
 
 void loop() {
-  int val = map(analogRead(pot), 0, 1024, 0, 255);
-  HC12.write("TEST 1234567890");
-  HC12.write(val);
-  Serial.println(val);
+  digitalWrite(LED_BUILTIN, (millis()/1000)%2); 
+  if (  (millis()/1000)%2 )   HC12.write("TEST 1234567890");
+
 }
